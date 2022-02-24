a90f64685093167bc23bd633b38984f6ac05ecfd
李金深 <2896583081@qq.com>
2022-02-24 16:31:55 +0800

    build(集成router和vuex): add router and vuex

---
+++ src/views/main/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>主页面</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="less" scoped></style>
